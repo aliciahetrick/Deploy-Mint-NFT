fc8b119cf133687656bfcfb86736f284bcf29f05
Alicia Hetrick <aliciahetrick@gmail.com>
2023-07-19 15:16:51 -0400

    Mint an NFT

---
+++ scripts/mint-nft.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const API_URL = process.env.API_URL;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+// console.log(JSON.stringify(contract.abi));
+const contractAddress = "0xB7bE5E8eEA0bF6b8715690fE52D455163356d4F1";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest");
+  // The nonce specification is used to keep track of the number of transactions sent from your address — which we need for security purposes and to prevent replay attacks(opens in a new tab)↗
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+}
+
+mintNFT("ipfs://QmbcDym7KqRkk4QuBeYoWFdvNeRW6MZxJodRBeQUXuriYe");
